14f5ef528fcdd4285eff69079a894281fbd89f73
VictorArmenta <armenta_victor@outlook.com>
2024-09-27 15:07:30 -0600

    Configuracion basica para usuario en memoria

---
+++ src/main/java/com/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.demo.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		UserDetails userDetails = User
+				.withUsername("user")
+				.password("pass123")
+				.roles("read").build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
